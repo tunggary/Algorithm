92c7ee94371afcfab088429b267d420ba583d9db
tunggary <gsy1656@gmail.com>
2022-03-07 10:04:01 +0900

    BOJ 2887

---
+++ 45-answer.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    n = int(input())
+    graph = [[False]*(n+1) for _ in range(n+1)]
+    data = list(map(int, input().split()))
+    for i in range(n):
+        for j in range(1,n):
+            graph[data[i]][]
+    
---
+++ graph/행성 터널/answer.py
@@ -0,0 +1,47 @@
+import heapq
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a > b:
+        parent[a] = b
+    else:
+        parent[b] = a
+
+n = int(input())
+x = []
+y = []
+z = []
+for i in range(1,n+1):
+    data = list(map(int, input().split()))
+    x.append((data[0], i))
+    y.append((data[1], i))
+    z.append((data[2], i))
+    
+x.sort()
+y.sort()
+z.sort()
+edges = []
+for i in range(n-1):
+    heapq.heappush(edges, (x[i+1][0] - x[i][0], x[i][1], x[i+1][1]))
+    heapq.heappush(edges, (y[i+1][0] - y[i][0], y[i][1], y[i+1][1]))
+    heapq.heappush(edges, (z[i+1][0] - z[i][0], z[i][1], z[i+1][1]))
+    
+parent = [0]*(n+1)
+for i in range(n+1):
+    parent[i] = i
+
+result = 0
+while edges:
+    cost, a, b = heapq.heappop(edges)
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
+    
