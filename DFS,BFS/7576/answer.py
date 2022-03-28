2e2a1e85535a5628d6afec90e5c1f7ca2ebc3e7b
tunggary <gsy1656@gmail.com>
2022-03-28 13:39:21 +0900

    BOJ 7576

--- DFS,BFS/7576/answer.py
+++ DFS,BFS/7576/answer.py
@@ -1,37 +1,42 @@
-from collections import deque
+#백준 7576번
+#아이디어: bfs에 대한 이해 확실히 해야함
+
 import sys
+from collections import deque
+
 m,n = map(int, sys.stdin.readline().split())
 graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+q = deque()
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
-q = deque()
 
 def check():
-  value = 0
+  max_day = 0
   for i in range(n):
     for j in range(m):
       if graph[i][j] == 0:
         return -1
-      value = max(value, graph[i][j])
-  return value - 1
+      max_day = max(max_day, graph[i][j])
+  return max_day - 1
 
-def bfs():  
+def bfs():
   while q:
-    cx,cy = q.popleft()
+    x,y = q.popleft()
     for i in range(4):
-      nx = cx + dx[i]
-      ny = cy + dy[i]
+      nx = x + dx[i]
+      ny = y + dy[i]
       if 0<=nx<n and 0<=ny<m:
-        if graph[nx][ny] == 0 or graph[nx][ny] > graph[cx][cy] + 1:
-           graph[nx][ny] = graph[cx][cy] + 1
-           q.append((nx,ny))
-
+        if graph[nx][ny] == 0:
+          q.append((nx,ny))
+          graph[nx][ny] = graph[x][y] + 1
+           
 for i in range(n):
   for j in range(m):
     if graph[i][j] == 1:
       q.append((i,j))
+      
 bfs()
-
 print(check())
-        
-    
+
+
+
