691f89adb60287d361952783c8f9c8c035ab643e
tunggary <gsy1656@gmail.com>
2022-04-06 11:33:54 +0900

    BOJ 7662

--- stack,heap,set,dict/7662/answer.py
+++ stack,heap,set,dict/7662/answer.py
@@ -2,17 +2,18 @@
 #아이디어: heap 두개를 어떻게 동기화 시킬것인가가 중요
 
 import sys
-from heapq import heappop, heappush
+from heapq import heappush, heappop
 
 for _ in range(int(sys.stdin.readline())):
+  n = int(sys.stdin.readline())
   max_heap = []
   min_heap = []
-  sync = [False]*1000001
-  for i in range(int(sys.stdin.readline())):
-    commmand, num = sys.stdin.readline().split()
-    if commmand == "I":
-      heappush(max_heap, (-int(num), i))
-      heappush(min_heap, (int(num), i))
+  sync = [False]*(n+1)
+  for i in range(n):
+    command, num = sys.stdin.readline().split()
+    if command == 'I':
+      heappush(max_heap, (-int(num),i))
+      heappush(min_heap, (int(num),i))
       sync[i] = True
     else:
       if num == '1':
@@ -22,17 +23,16 @@ for _ in range(int(sys.stdin.readline())):
           sync[max_heap[0][1]] = False
           heappop(max_heap)
       else:
-        while min_heap and not sync[min_heap[0][1]]:
-          heappop(min_heap)
-        if min_heap:
-          sync[min_heap[0][1]] = False
-          heappop(min_heap)
-          
+          while min_heap and not sync[min_heap[0][1]]:
+            heappop(min_heap)
+          if min_heap:
+            sync[min_heap[0][1]] = False
+            heappop(min_heap)
   while max_heap and not sync[max_heap[0][1]]:
     heappop(max_heap)
   while min_heap and not sync[min_heap[0][1]]:
     heappop(min_heap)
-  if min_heap and max_heap:
+  if max_heap and min_heap:
     print(-max_heap[0][0], min_heap[0][0])
   else:
     print("EMPTY")
